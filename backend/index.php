74e9fdbb8f21d72147a8ce2328aa61f92300cee1
Diogo-MD <diogomdossantos@gmail.com>
2024-05-14 22:12:52 -0300

    Update DB e DOC's

---
+++ backend/index.php
@@ -0,0 +1,9 @@
+<?php
+require_once("template/header.php");
+?>
+
+<h2>Projeto Mapa Sala Senac</h2>
+
+<?php
+require_once("template/footer.php");
+?>
---
+++ backend/template/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema Vendas</title>
+    
+    <ul>
+        <li>aaa</li>
+        <li>asda</li>
+    </ul>
+
+</head>
+
+<body>
+    <header>
+
+    </header>
+    
